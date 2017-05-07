b8b42e39d8f81f23a523a223db02e42997e5a294
Toygar Onbay <toygaronbay@Toygars-MacBook-Pro.local>
2017-05-07 14:08:47 +0800

    first commit

--- src/states/Main.js
+++ src/states/Main.js
@@ -1,21 +1,17 @@
 import ExampleObject from 'objects/ExampleObject';
 
+let mysprite;
+
 class Main extends Phaser.State {
 
 	create() {
 
-		//Enable Arcade Physics
-		this.game.physics.startSystem(Phaser.Physics.ARCADE);
-
-		//Set the games background colour
-		this.game.stage.backgroundColor = '#cecece';
-
-		//Example of including an object
-		//let exampleObject = new ExampleObject(this.game);
 	}
 
 	update() {
-		
+		mysprite = this.game.add.sprite(15, 30, 'myguy');
+		mysprite.animations.add('left', [21, 22, 23, 24, 25, 26, 27], 10, true);
+		mysprite.animations.play('left');
 	}
 
 }
--- src/states/Preload.js
+++ src/states/Preload.js
@@ -5,6 +5,8 @@ class Preload extends Phaser.State {
 		//this.game.load.image('myImage', 'assets/my-image.png');
 		//this.game.load.audio('myAudio', 'assets/my-audio.wav');
 		//this.game.load.atlas('myAtlas', 'assets/my-atlas.png', 'assets/my-atlas.json');
+		//
+		this.game.load.spritesheet('myguy', 'assets/player/player-spritemap-v9.png', 46, 50);
 	}
 
 	create() {
